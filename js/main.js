d89f89044968b8654b6962cbbdd4998cfcbd78a0
Priscilla Haibara <megumi.ufrn@gmail.com>
2025-08-02 14:37:38 -0300

    feat(header): add responsive mobile menu

--- js/main.js
+++ js/main.js
@@ -1,6 +1,18 @@
+/* ====== SHOW MENU ======  */
 
+const toggle = document.querySelector('.header__toggle')
+const nav = document.querySelector('.header__nav')
 
+function showMenu() {
+    toggle.addEventListener('click', () => {
+        /* Add show-menu class to nav menu */
+        nav.classList.toggle('show-menu');
 
+        /* Add show-icon to show and hide the menu icon */
+        toggle.classList.toggle('show-icon')
+    })
+}
 
+showMenu();
 
 
