0d5ba3f10571cdae71705d019db79567f167126b
Priscilla Haibara <megumi.ufrn@gmail.com>
2025-08-04 17:09:52 -0300

    refactor(main): split JS into modules

--- js/main.js
+++ js/main.js
@@ -1,92 +1,10 @@
-/* ====== SHOW MENU ======  */
+import menuToggle from './menuToggle.js'
+import typingHero from './typingHero.js';
+import skillsObserver from './skillsObserver.js';
+import videoObserver from './videoObserver.js';
 
-const toggle = document.querySelector(".header__toggle");
-const nav = document.querySelector(".header__nav");
+menuToggle(); /* Toggles the mobile navigation menu when the toggle button is clicked. */
+skillsObserver(); // Observe elements in skills section and trigger animations
+typingHero(); /* Animates typing and deleting effect for a set of texts in a loop. */
+videoObserver(); /* Sets up an IntersectionObserver to asynchronously load the background video */
 
-function showMenu() {
-  toggle.addEventListener("click", () => {
-    /* Add show-menu class to nav menu */
-    nav.classList.toggle("show-menu");
-
-    /* Add show-icon to show and hide the menu icon */
-    toggle.classList.toggle("show-icon");
-  });
-}
-
-showMenu();
-
-/* ====== SECTION HERO ANIMATION ======  */
-const element = document.querySelector(".animated-text");
-
-const texts = ["Software Engineer.", "Front-end Devoloper.", "Medical Doctor."];
-let textIndex = 0;
-let charIndex = 0;
-let isDeleting = false;
-
-function type() {
-  const currentText = texts[textIndex];
-  const speed = isDeleting ? 50 : 120;
-
-  element.textContent = currentText.substring(0, charIndex);
-
-  if (!isDeleting && charIndex < currentText.length) {
-    charIndex++;
-  } else if (isDeleting && charIndex > 0) {
-    charIndex--;
-  } else if (!isDeleting) {
-    isDeleting = true;
-    setTimeout(type, 2000);
-    return;
-  } else {
-    isDeleting = false;
-    textIndex = (textIndex + 1) % texts.length;
-  }
-  setTimeout(type, speed);
-}
-
-document.addEventListener("DOMContentLoaded", type);
-
-/* ====== SECTION SKILLS ANIMATION ======  */
-const allHiddenSkills = document.querySelectorAll(".chart, .skills__content");
-
-const skillsObserver = new IntersectionObserver(
-  (entries) => {
-    entries.forEach((entry) => {
-      if (!entry.isIntersecting) return;
-
-      if (entry.target.classList.contains("hidden--right")) {
-        entry.target.style.opacity = 1;
-        entry.target.classList.add("animated--right");
-      } else if (entry.target.classList.contains("hidden--left")) {
-        entry.target.style.opacity = 1;
-        entry.target.classList.add("animated--left");
-      }
-      skillsObserver.unobserve(entry.target)
-    });
-  },
-  { threshold: 0.5 }
-);
-
-allHiddenSkills.forEach((skill) => skillsObserver.observe(skill));
-
-/* ASYNCHRONOUS LOADING OF BACKGROUND VIDEO */
-const video = document.querySelector(".projects__background-video");
-const source = video.querySelector("source");
-
-const videoObserver = new IntersectionObserver(
-  (entries) => {
-    entries.forEach((entry) => {
-      if (!entry.isIntersecting) return;
-      
-      const videoSrc = source.getAttribute('data-src');
-      if (videoSrc) {
-        source.setAttribute('src', videoSrc);
-        video.load();
-      }
-      videoObserver.unobserve(video);
-    });
-  },
-  { threshold: 0 }
-);
-
-videoObserver.observe(video);
---
+++ js/menuToggle.js
@@ -0,0 +1,13 @@
+/* ====== SHOW MENU ======  */
+const toggle = document.querySelector(".header__toggle");
+const nav = document.querySelector(".header__nav");
+
+export default function menuToggle() {
+  toggle.addEventListener("click", () => {
+    /* Add show-menu class to nav menu */
+    nav.classList.toggle("show-menu");
+
+    /* Add show-icon to show and hide the menu icon */
+    toggle.classList.toggle("show-icon");
+  });
+}
