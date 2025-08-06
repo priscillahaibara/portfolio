554266d6dab92e504a314d9afcaa74a7d6cabd56
Priscilla Haibara <megumi.ufrn@gmail.com>
2025-08-06 13:38:56 -0300

    chore: remove typing animation and intersection observer for background video due to performance issues

--- js/main.js
+++ js/main.js
@@ -1,10 +1,6 @@
 import menuToggle from './menuToggle.js'
-import typingHero from './typingHero.js';
 import skillsObserver from './skillsObserver.js';
-import videoObserver from './videoObserver.js';
 
 menuToggle(); /* Toggles the mobile navigation menu when the toggle button is clicked. */
 skillsObserver(); // Observe elements in skills section and trigger animations
-typingHero(); /* Animates typing and deleting effect for a set of texts in a loop. */
-videoObserver(); /* Sets up an IntersectionObserver to asynchronously load the background video */
 
